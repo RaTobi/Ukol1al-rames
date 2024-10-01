0b66907cdec53649d0d0b21ad055eff8cdeca7a7
RaTobi <159123966+RaTobi@users.noreply.github.com>
2024-10-01 23:29:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+arrayA = [10, 20, 30, 40, 50, 60, 70, 80, 90] #ukol cislo 1
+
+print(arrayA[0],",",arrayA[4],",",arrayA[8]) #ukol cislo 2
+
+arrayA[5] = 34 # ukol cislo 3
+
+print(arrayA[7]) #ukol cislo 4
+
+print (len(arrayA)) #ukol cislo 5
+
+print(arrayA[0], ",", arrayA[8]) #ukol cislo 6
+
+arrayB = [7, 17, 6, 55, 25, 65] #ukol cislo 7
+
+print (sum(arrayB)) #ukol cislo 8
+
+print(arrayB[1] + arrayB[5])#ukol cislo 9

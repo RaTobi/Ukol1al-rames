8207841caa455d558941006783bcfaa1ea3094d1
RaTobi <159123966+RaTobi@users.noreply.github.com>
2024-10-09 11:20:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 arrayA = [10, 20, 30, 40, 50, 60, 70, 80, 90] #ukol cislo 1
 
-print(arrayA[0],",",arrayA[4],",",arrayA[8]) #ukol cislo 2
+serazene_pole = sorted(arrayA)
+prostredni_hodnota = serazene_pole[len(serazene_pole) // 2]
 
-arrayA[5] = 34 # ukol cislo 3
 
-print(arrayA[7]) #ukol cislo 4
+print(arrayA[0],",",prostredni_hodnota,",",arrayA[8]) #ukol cislo 2
+
+arrayA[6] = 34 # ukol cislo 3
+
+print(arrayA[8]) #ukol cislo 4
 
 print (len(arrayA)) #ukol cislo 5
 
@@ -14,4 +18,4 @@ arrayB = [7, 17, 6, 55, 25, 65] #ukol cislo 7
 
 print (sum(arrayB)) #ukol cislo 8
 
-print(arrayB[1] + arrayB[5])#ukol cislo 9
+print(arrayB[0] + arrayB[5])#ukol cislo 9

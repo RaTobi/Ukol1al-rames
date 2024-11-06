66cd819fcc60f16de586b195c8b8015865516e59
RaTobi <159123966+RaTobi@users.noreply.github.com>
2024-11-06 10:18:13 +0000

    final_rames

--- jprames.py
+++ jprames.py
@@ -1,1 +1,11 @@
-
+array = ["audi","bmw","mercedes","porsche","skoda"]
+print(array)
+array.append("vítr")
+print(array)
+array.remove("bmw")
+print(array)
+array[4]=("slunce")
+print(array)
+arrayNmubers = ["honda", "subaru"]
+array.extend(arrayNmubers)
+print(array)
